42e9dc5fda259623597e3803f827e823296c2412
Charles DeBrosse <cmdebrosse@outlook.com?>
2022-03-27 16:23:42 -0400

    completed skills component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { BsSkipEndFill } from "react-icons/bs";
 import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Skills />
     </div>
   );
 }
